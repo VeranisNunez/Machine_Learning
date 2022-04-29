519f20fd5fea52a93f8679750d971c4cc169a926
Veranis Nuñez <Veranisnb@gmail.com>
2022-04-29 00:07:13 -0500

    Bank Marketing - Dividir la data en dos

--- Bank Marketing/bank-full.py
+++ Bank Marketing/bank-full.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Leer data
 url = 'bank-full.csv'
@@ -23,3 +24,21 @@ names = ['1', '2', '3', '4', '5', '6', '7']
 data.age = pd.cut(data.age, ranges, labels=names)
 
 data.dropna(axis=0, how='any', inplace=True)
+
+
+# Dividir la data en dos
+data_train = data[:22604]
+data_test = data[22604:]
+
+x = np.array(data_train.drop(['y'], 1))
+y = np.array(data_train.y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+x_test_out = np.array(data_test.drop(['y'], 1))
+y_test_out = np.array(data_test.y)
+
+
+
+
+
