e075d297e47e655845d5ce898c45864ce2b6f994
Veranis Nuñez <Veranisnb@gmail.com>
2022-04-29 00:25:10 -0500

    Diabetes - Dividir la data en dos

--- Prima Indians Diabetes Database/diabetes.py
+++ Prima Indians Diabetes Database/diabetes.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 # Leer data
 url = 'diabetes.csv'
 data = pd.read_csv(url)
+
 
 # Tratamiento de la data
 data.drop(['Pregnancies', 'BloodPressure', 'SkinThickness'], axis= 1, inplace = True)
@@ -12,3 +15,16 @@ rangos = [0, 8, 15, 18, 25, 40, 60, 100]
 nombres = ['1', '2', '3', '4', '5', '6', '7']
 data.Age = pd.cut(data.Age, rangos, labels=nombres)
 data.dropna(axis=0,how='any', inplace=True)
+
+
+# Dividir la data en dos
+data_train = data[:384]
+data_test = data[384:]
+
+x = np.array(data_train.drop(['Outcome'], axis=1))
+y = np.array(data_test.Outcome)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+x_test_out = np.array(data_test.drop(['Outcome'], axis=1))
+y_test_out = np.array(data_test.Outcome)
